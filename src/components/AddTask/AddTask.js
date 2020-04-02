1a8136727bb120bb7e476098452de877fba11667
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-04-02 12:40:43 +0200

    Add AddTask file

---
+++ src/components/AddTask/AddTask.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddTask extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default AddTask;
