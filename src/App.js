d35c9b563df2fa82dc1eae0b708336470c01f0f2
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-04-02 12:19:08 +0200

    Add some example default tasks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  counter = 6;
+  state = {
+    tasks: [
+      {
+        id: 0,
+        text: 'walk a dog',
+        important: true,
+        active: true
+      },
+      {
+        id: 1,
+        text: 'study programming',
+        important: true,
+        active: true
+      },
+      {
+        id: 2,
+        text: 'workout',
+        important: false,
+        active: true
+      },
+      {
+        id: 3,
+        text: 'play video games',
+        important: false,
+        active: true
+      },
+      {
+        id: 4,
+        text: 'buy groceries',
+        important: true,
+        active: true
+      },
+      {
+        id: 5,
+        text: 'do laundry',
+        important: false,
+        active: true
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
