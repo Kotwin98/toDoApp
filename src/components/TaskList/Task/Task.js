698d8c030336b7bfdcfecd8771dc3bcd759917d4
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-04-03 12:22:30 +0200

    Return single task view

--- src/components/TaskList/Task/Task.js
+++ src/components/TaskList/Task/Task.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
 const Task = props => {
-    
+    const style = {
+        color: '#'
+    };
+    const { text, id, active, important } = props.task;
+    if (active) {
+        return (
+            <div>
+                <p>
+                    <strong style={important ? style : null}>{text}</strong>
+                    <button onClick={() => props.change(id)}>
+                        Done
+                    </button>
+                    <button onClick={() => props.delete(id)}>
+                        x
+                    </button>
+                </p>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <p>
+                    <strong>{text}</strong>
+                    <button onClick={() => props.delete(id)}>
+                        x
+                    </button>
+                </p>
+            </div>
+        );
+    }
 };
 
 export default Task;
