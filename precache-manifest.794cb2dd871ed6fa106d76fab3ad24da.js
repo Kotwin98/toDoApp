129828473bf2019ebe62aabce162c6f06701bc7a
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-04-07 13:10:26 +0200

    Updates

---
+++ precache-manifest.794cb2dd871ed6fa106d76fab3ad24da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6323c2029603142efe67f934ef20dfce",
+    "url": "/toDoApp/index.html"
+  },
+  {
+    "revision": "b9a35f5c894796375641",
+    "url": "/toDoApp/static/css/main.e8a76dc7.chunk.css"
+  },
+  {
+    "revision": "1b4e1855c5711f82e7e2",
+    "url": "/toDoApp/static/js/2.c7ff102e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoApp/static/js/2.c7ff102e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a35f5c894796375641",
+    "url": "/toDoApp/static/js/main.479f1b94.chunk.js"
+  },
+  {
+    "revision": "1049cfe30605380addae",
+    "url": "/toDoApp/static/js/runtime-main.bee7a9fd.js"
+  }
+]);
